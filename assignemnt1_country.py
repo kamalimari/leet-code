97e6986e99b07c8539868d01203f43b64572cf20
kamalimari <kamalimari@rediffmail.com>
2019-04-17 16:25:17 +0530

    build_area_greater_than_4000

--- assignemnt1_country.py
+++ assignemnt1_country.py
@@ -1,6 +1,31 @@
 import pandas
-df = pandas.read_csv('exercise_1_location.csv')
-print(df)
-def function():
+df = pandas.read_csv('exercise_1_store.csv')
+store_name = []
+build_area = []
+date_of_area = []
+pincode = []
+
+
 for index, row in df.iterrows():
-    print(row['pincode'], row['city'])
+    store_name.append(row[0])
+    build_area.append(row[1])
+    date_of_area.append(row[2])
+    pincode.append(row[3])
+
+
+def build_area_greater_than_4000():
+    build_area_greater_than_4000_ = []
+    for i in range(len(build_area)):
+        if build_area[i] > 4000:
+            build_area_greater_than_4000_.append(store_name[i])
+    print(build_area_greater_than_4000_)
+
+
+build_area_greater_than_4000()
+
+
+
+
+    # print(index['pincode'], index['city'])
+
+
